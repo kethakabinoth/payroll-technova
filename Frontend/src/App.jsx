3a7502e2fb679a907f9bf66456d0063216a74044
Ketha2002 <kbinoth6@gmail.com>
2025-08-13 11:42:52 +0530

    Add App component with SignIn, SignUp, and SidebarPage integration

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignIn from './Pages/Signin'
+import SignUp from './Pages/Signup'
+import SidebarPage from './Pages/Sidebar'
+
+function App() {
+  return (
+    <div>
+      <SignIn />
+      <SignUp/>
+      <SidebarPage/>
+    </div>
+  )
+}
+
+export default App
