4fa04ff19a8dddbb5c803d38c4bb0025a9ac05cb
Ketha2002 <kbinoth6@gmail.com>
2025-08-14 13:42:12 +0530

    add routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import SignIn from './Pages/Signin'
 import SignUp from './Pages/Signup'
 import SidebarPage from './Pages/Sidebar'
+import CompanyRegister from './Pages/CompanyRegister'
+import BackupDatabase from './Pages/BackupDatabase'
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <SignIn />
       <SignUp/>
       <SidebarPage/>
+      <CompanyRegister />
+      <BackupDatabase />
+      
     </div>
   )
 }
