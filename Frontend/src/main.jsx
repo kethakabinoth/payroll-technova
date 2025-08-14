5f9ffd0eaf72ebaf62cb403aee750b156a191f5b
Ketha2002 <kbinoth6@gmail.com>
2025-08-14 13:42:40 +0530

    set route path for CompanyRegister and BackupDatabase

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import SignUp from './Pages/Signup.jsx';
 import SignIn from './Pages/Signin.jsx';
 import SidebarPage from './Pages/Sidebar.jsx';
+import CompanyRegister from './Pages/CompanyRegister.jsx';
+import BackupDatabase from './Pages/BackupDatabase.jsx';
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Routes>
@@ -13,6 +16,9 @@ createRoot(document.getElementById('root')).render(
     <Route path="/signup" element={<SignUp />} />
     <Route path="/signin" element={<SignIn />} />
     <Route path="/sidebar" element={<SidebarPage />} />
+    <Route path="/company-register" element={<CompanyRegister />} />
+    <Route path="/backup-database" element={<BackupDatabase />} />
+    
   </Routes>
   </BrowserRouter>
 );
