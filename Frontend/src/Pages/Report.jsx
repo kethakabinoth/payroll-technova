0315db224154d8d9e77d4f42e4c28e2babe531fe
Ketha2002 <kbinoth6@gmail.com>
2025-08-15 14:20:16 +0530

    create Report page

---
+++ Frontend/src/Pages/Report.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Report() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center my-8">Report Page</h1>
+      <p className="text-center text-gray-600">This is the report page where you can view various reports.</p>
+      {/* Additional report content can be added here */}
+    </div>
+  )
+}
+
+export default Report
